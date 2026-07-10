5bca51167b0adac7928fc242a7f794334a505b71
aapdo <jade04342@gmail.com>
2026-07-10 09:58:19 +0900

    Use SPD dataset builder for H200 evaluation

--- tools/test.py
+++ tools/test.py
@@ -15,7 +15,7 @@ from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import (get_dist_info, init_dist, load_checkpoint,
                          wrap_fp16_model)
 
-from mmdet3d.datasets import build_dataset
+from projects.mmdet3d_plugin.datasets import custom_build_dataset
 from projects.mmdet3d_plugin.datasets.builder import build_dataloader
 from mmdet3d.models import build_model
 from mmdet.apis import set_random_seed
@@ -196,7 +196,7 @@ def main():
         set_random_seed(args.seed, deterministic=args.deterministic)
 
     # build the dataloader
-    dataset = build_dataset(cfg.data.test)
+    dataset = custom_build_dataset(cfg.data.test)
     data_loader = build_dataloader(
         dataset,
         samples_per_gpu=samples_per_gpu,

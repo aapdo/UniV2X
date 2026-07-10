8e8b3026f07a9f1bec59799937ac89b3425f32ae
aapdo <jade04342@gmail.com>
2026-07-10 09:52:59 +0900

    Remove unused legacy single-GPU test import

--- tools/test.py
+++ tools/test.py
@@ -15,7 +15,6 @@ from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import (get_dist_info, init_dist, load_checkpoint,
                          wrap_fp16_model)
 
-from mmdet3d.apis import single_gpu_test
 from mmdet3d.datasets import build_dataset
 from projects.mmdet3d_plugin.datasets.builder import build_dataloader
 from mmdet3d.models import build_model
